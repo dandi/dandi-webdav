efd3603e9f5f33a2d9ad86245b0efdb48856c909
John T. Wodder II <git@varonathe.org>
2024-01-08 15:56:33 -0500

    Packaging

---
+++ src/dandidav/__init__.py
@@ -0,0 +1,11 @@
+"""
+WebDAV view to DANDI Archive
+
+Visit <https://github.com/dandi/dandi-webdav> for more information.
+"""
+
+__version__ = "0.0.0"
+__author__ = "DANDI Developers"
+__author_email__ = "team@dandiarchive.org"
+__license__ = "MIT"
+__url__ = "https://github.com/dandi/dandi-webdav"
